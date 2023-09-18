95850ef16a3c1d8a4656bb3587d59b14e0cb5f2b
MrBadshah69 <mrbadshahbusiness@gmail.com>
2023-09-18 23:28:50 +0500

    Muhammad Arsalan Portfolio

--- assets/js/MouseCursor.js
+++ assets/js/MouseCursor.js
@@ -1,20 +1,24 @@
 const cursordot = document.querySelector("[data-cursor-dot]");
 const cursoroutline = document.querySelector("[data-cursor-outline]");
 
-window.addEventListener("mousemove" , function(e){
-    const posX = e.clientX;
-    const posY = e.clientY;
+window.addEventListener("mousemove", function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
 
-    cursordot.style.left = `${posX}px`; 
-    cursordot.style.top = `${posY}px`;
-    
-    // cursoroutline.style.left = `${posX}px`;
-    // cursoroutline.style.top = `${posY}px`;
+  cursordot.style.left = `${posX}px`;
+  cursordot.style.top = `${posY}px`;
 
-    cursoroutline.animate({
-        left: `${posX}px`,
-        top: `${posY}px`
-    } ,{ duration: 500, fill: "forwards"
+  // cursoroutline.style.left = `${posX}px`;
+  // cursoroutline.style.top = `${posY}px`;
 
-        });
-    });
+  cursoroutline.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    {
+      duration: 500,
+      fill: "forwards",
+    }
+  );
+});
